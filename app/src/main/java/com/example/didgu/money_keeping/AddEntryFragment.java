88e94b995ce285a32bf1bb427a081366f94852f5
Hyun-Bum Yang <hyunbumy@usc.edu>
2017-07-01 16:02:51 -0700

    Shows toast if the new entry form is not filled out correctly:

--- app/src/main/java/com/example/didgu/money_keeping/AddEntryFragment.java
+++ app/src/main/java/com/example/didgu/money_keeping/AddEntryFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.util.Date;
@@ -27,13 +28,20 @@ public class AddEntryFragment extends Fragment {
 
     // RootView that the Fragment is in
     View rootView;
+    EditText dateEditText;
+    EditText nameEditText;
+    EditText amountEditText;
+    EditText descEditText;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_newentry, container, false);
 
-        EditText dateEditText = (EditText) rootView.findViewById(R.id.expenditure_date);
+        dateEditText = (EditText) rootView.findViewById(R.id.expenditure_date);
+        nameEditText = (EditText) rootView.findViewById(R.id.expenditure_name);
+        amountEditText = (EditText) rootView.findViewById(R.id.money_amount);
+        descEditText = (EditText) rootView.findViewById(R.id.description);
 
         // Sets onClickListener for date field click
         dateEditText.setOnClickListener(new View.OnClickListener() {
@@ -54,8 +62,11 @@ public class AddEntryFragment extends Fragment {
             public void onClick(View v) {
                 try {
                     addExpend();
+                    clearFields();
                 } catch (ParseException e) {
                     e.printStackTrace();
+                    Toast.makeText(getActivity(), "Invalid Date format", Toast.LENGTH_SHORT).show();
+                    clearFields();
                 }
             }
         });
@@ -70,18 +81,25 @@ public class AddEntryFragment extends Fragment {
      */
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void addExpend() throws ParseException {
-        String name = ((EditText) rootView.findViewById(R.id.expenditure_name)).getText().toString();
+        String name = nameEditText.getText().toString();
+        String amount_str = amountEditText.getText().toString();
+        String date_str = dateEditText.getText().toString();
+        String description = descEditText.getText().toString();
 
-        String amount_str = ((EditText) rootView.findViewById(R.id.money_amount)).getText().toString();
+        // Checks if the fields are all filled out
+        if (name.equals("") || amount_str.equals("") ||
+                date_str.equals("") || description.equals(""))
+        {
+            Toast.makeText(getActivity(), "Please fill out the form", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Parse into type float
         float amount = Float.parseFloat(amount_str);
 
         // parses and formats the date and converts into Date class
-        String date_str = ((EditText) rootView.findViewById(R.id.expenditure_date)).getText().toString();
         DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
         Date date = df.parse(date_str);
-
-        EditText desc_field = (EditText) rootView.findViewById(R.id.description);
-        String description = desc_field.getText().toString();
 
         // Add to the ArrayList
         Expenditure newExpenditure = Expenditure.createExpenditure(name, amount, date, description);
@@ -91,6 +109,17 @@ public class AddEntryFragment extends Fragment {
          */
         //NumberFormat form = NumberFormat.getCurrencyInstance();
         //String temp = form.format(amount);
+    }
+
+    /**
+     * Resets the fields
+     */
+    private void clearFields()
+    {
+        nameEditText.setText("");
+        amountEditText.setText("");
+        dateEditText.setText("");
+        descEditText.setText("");
     }
 
 }
