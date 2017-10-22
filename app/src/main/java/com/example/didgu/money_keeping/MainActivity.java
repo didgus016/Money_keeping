8237d77e1b564f6863f07aef5c58d749c67afddf
Hyun-Bum Yang <hyunbumy@usc.edu>
2017-10-21 19:38:01 -0700

    Add sign out functionality

--- app/src/main/java/com/example/didgu/money_keeping/MainActivity.java
+++ app/src/main/java/com/example/didgu/money_keeping/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.didgu.money_keeping;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity{
+
+    TextView mUserTab;
+    FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -25,5 +35,18 @@ public class MainActivity extends AppCompatActivity{
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        mUserTab = (TextView) findViewById(R.id.current_user);
+        mUserTab.setText(mUser.getEmail());
+
+        Button signOutButton = (Button) findViewById(R.id.sign_out);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
